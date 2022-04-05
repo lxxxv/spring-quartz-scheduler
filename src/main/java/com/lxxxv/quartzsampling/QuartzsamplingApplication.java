6b3a8c93146d53e2271e593d18fbc0753211b5ef
lxxxv <justcodeit.py@gmail.com>
2022-04-05 09:52:36 +0900

    -

---
+++ src/main/java/com/lxxxv/quartzsampling/QuartzsamplingApplication.java
@@ -0,0 +1,13 @@
+package com.lxxxv.quartzsampling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzsamplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzsamplingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lxxxv/quartzsampling/QuartzsamplingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxxxv.quartzsampling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzsamplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
